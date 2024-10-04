11c80e1b3507876f3bddab934748e5168f77478d
Anthony Loor <111461610+anthonyloor@users.noreply.github.com>
2024-10-04 17:43:40 +0200

    [add] first components

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'gray-light': '#f7f7f7',
+      },
+    },
   },
   plugins: [],
-}
-
+};
